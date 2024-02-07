8576bbee78463224318f4fc8db9ed520ec59b366
ChoiAnYong <dksdyd78@naver.com>
2024-02-07 22:23:55 +0900

    주문 진행 정보 구현중

--- FastCampusRiders/Presentaion/OrderTracking/OrderTrackingViewController.swift
+++ FastCampusRiders/Presentaion/OrderTracking/OrderTrackingViewController.swift
@@ -30,4 +30,34 @@ class OrderTrackingViewController: UIViewController {
     @IBOutlet private weak var progressDelivaryConstraint: NSLayoutConstraint!
     @IBOutlet private weak var progressCompletionConstraint: NSLayoutConstraint!
 
+    private var defaultColor: UIColor {
+        return UIColor(red: 0.824, green: 0.824, blue: 0.824, alpha: 1)
+    }
+
+    private var highlightedColor: UIColor {
+        return UIColor(red: 0.157, green: 0.761, blue: 0.737, alpha: 1)
+    }
+
+    override func viewDidLoad() {
+        self.startPoint.layer.cornerRadius = 4.5
+        self.midPoint.layer.cornerRadius = 4.5
+        self.endPoint.layer.cornerRadius = 4.5
+    }
 }
+
+extension OrderTrackingViewController {
+    private func setStartStatus() {
+        self.indicatorStartConstraint.priority = .defaultHigh
+        self.indicatorDelivaryConstraint.priority = .defaultLow
+        self.indicatorCompletionConstraint.priority = .defaultLow
+        
+        self.progressStartConstraint.priority = .defaultHigh
+        self.progressDelivaryConstraint.priority = .defaultLow
+        self.progressCompletionConstraint.priority = .defaultLow
+        
+        self.startLabel.textColor = self.highlightedColor
+        self.midLabel.textColor = self.defaultColor
+        self.endLabel.textColor = self.defaultColor
+    }
+}
+
